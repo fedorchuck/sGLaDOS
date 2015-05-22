09dce9a4e84ae77c8051ca0a9c1ae384157159bf
fedorchuck <vl.fedorchuck@gmail.com>
2015-05-22 14:01:17 +0300

    added helper units (for throwable)

---
+++ sGLaDOS_server/src/main/java/io.github.fedorchuck.sglados_server/helperUnits/ThrowThrowable.java
@@ -0,0 +1,17 @@
+package io.github.fedorchuck.sglados_server.helperUnits;
+
+/**
+ * Created by v on 22.05.2015.
+ */
+public class ThrowThrowable {
+    public <Th extends Throwable, Ob extends Object, Wh extends Object> void throwError(
+            final Ob problemClass,
+            final Th error,
+            final Wh what) {
+        System.err.println("in class: " +  problemClass);
+        System.err.println(error.getClass().getName() + ": " + error.getMessage());
+        System.err.println(what);
+        //throw error;
+        //System.exit(-1);
+    }
+}
